c2f7e0f7cb200c4b4035baf00c8a337bae4dddec
robert.sheline <robert.sheline@soartech.com>
2018-06-06 11:45:42 -0400

    changed template

---
+++ custom.py
@@ -0,0 +1,137 @@
+# this file imports custom routes into the experiment server
+
+from flask import Blueprint, render_template, request, jsonify, Response, abort, current_app, flash
+from jinja2 import TemplateNotFound
+from functools import wraps
+from sqlalchemy import or_, and_
+from sqlalchemy.orm.exc import NoResultFound
+
+from psiturk.psiturk_config import PsiturkConfig
+from psiturk.experiment_errors import ExperimentError
+from psiturk.user_utils import PsiTurkAuthorization, nocache
+
+# # Database setup
+from psiturk.db import db_session, init_db
+from psiturk.models import Participant
+from json import dumps, loads
+from custom_models import LegitWorker
+import datetime
+
+from flask_socketio import SocketIO
+
+# load the configuration options
+config = PsiturkConfig()
+config.load_config()
+myauth = PsiTurkAuthorization(config)  # if you want to add a password protect route use this
+
+# explore the Blueprint
+custom_code = Blueprint('custom_code', __name__, template_folder='templates', static_folder='static')
+socketio = SocketIO(engineio_logger=True)
+###########################################################
+#  serving warm, fresh, & sweet custom, user-provided routes
+#  add them here
+###########################################################
+
+# Status codes
+NOT_ACCEPTED = 0
+ALLOCATED = 1
+STARTED = 2
+COMPLETED = 3
+SUBMITTED = 4
+CREDITED = 5
+QUITEARLY = 6
+BONUSED = 7
+
+@socketio.on('joined')
+def test_socket():
+    pass
+
+#----------------------------------------------
+# example using HTTP authentication
+#----------------------------------------------
+@custom_code.route('/dashboard', methods=['GET','POST'])
+@myauth.requires_auth
+def dashboard():
+    if 'mode' in request.form:
+        if request.form['mode']=='add':
+            if ('workerid' in request.form) and ('bonus' in request.form):
+                if (LegitWorker.query.filter(LegitWorker.amt_worker_id == request.form['workerid']).count() == 0):
+                    newworker = LegitWorker(workerid=request.form['workerid'])
+                    newworker.set_bonus(float(request.form['bonus']))
+                    db_session.add(newworker)
+                    db_session.commit()
+                else:
+                    flash('That worker has already been added!', 'error')
+        elif request.form['mode']=='delete':
+            if ('index' in request.form):
+                current_app.logger.info('deleting')
+                try:
+                    lw=LegitWorker.query.filter(LegitWorker.index == int(request.form['index'])).one()
+                    db_session.delete(lw)
+                    db_session.commit()
+                except:
+                    flash(u'Sorry, was unable to delete that worker.  Perhaps they were already deleted!', 'error')
+        elif request.form['mode']=='refresh':
+            failed_workers = []
+            workers = LegitWorker.query.all()
+            for lw in workers:
+                try:
+                    user = Participant.query.filter(Participant.workerid == lw.amt_worker_id).one()
+                    if user.status == BONUSED:
+                        try:
+                            lw.paid()
+                            db_session.add(lw)
+                            db_session.commit()
+                        except Exception as ex:
+                            current_app.logger.error('Could not update worker %s to paid status: %s',
+                                                lw.amt_worker_id,
+                                                ex)
+                            failed_workers.append(w.amt_worker_id)  
+                except NoResultFound:
+                    pass # hasn't submitted yet... 
+                if len(failed_workers) > 0:
+                    display_str = u'Could not update the following workers:'
+                    for w in failed_workers:
+                        display_str += '\n%s' % (w)
+                    flash(display_str, 'error')
+    try:
+        workers = LegitWorker.query.all()
+        return render_template('dashboard.html', workers = workers)
+    except TemplateNotFound:
+        abort(404)
+
+#----------------------------------------------
+# verify secret code
+#----------------------------------------------
+@custom_code.route('/check_flask', methods=['POST'])
+def check_secret_code():
+    current_app.logger.info(request.form['workerid'])
+    current_app.logger.info(request.form['code'])
+    # uniqueId = request.form['uniqueid']
+    # try:
+    #     worker = LegitWorker.query.filter(and_(LegitWorker.amt_worker_id ==request.form['workerid'], \
+    #                                 LegitWorker.completion_code == request.form['code'], \
+    #                                 LegitWorker.status=='owed')).one()
+    # except:
+    #     abort(406)
+    # worker.submitted()
+    # db_session.add(worker)
+    # db_session.commit()
+    #
+    # try:
+    #     user = Participant.query.\
+    #             filter(Participant.uniqueid == uniqueId).one()
+    #     user.bonus = worker.bonus
+    #     user.status = COMPLETED
+    #     user.endhit = datetime.datetime.now()
+    #     db_session.add(user)
+    #     db_session.commit()
+    # except:
+    #     abort(406)
+    # resp = {"bonus": user.bonus}
+    # return jsonify(**resp)
+    return "your worker ID is " + str(request.form['workerid'])
+
+
+
+    
---
+++ static/js/task.js
@@ -0,0 +1,81 @@
+/*
+ * Requires:
+ *     psiturk.js
+ *     utils.js
+ */
+
+// Initalize psiturk object
+var psiTurk = new PsiTurk(uniqueId, adServerLoc, mode);
+
+// they are not used in the stroop code but may be useful to you
+
+// All pages to be loaded
+var pages = [
+	"form.html",
+	"success.html"
+];
+
+psiTurk.preloadPages(pages);
+
+
+/********************
+* HTML manipulation
+*
+* All HTML files in the templates directory are requested 
+* from the server when the PsiTurk object is created above. We
+* need code to get those pages from the PsiTurk object and 
+* insert them into the document.
+*
+********************/
+
+checkcodesuccess = function (bonus) {
+	psiTurk.showPage('success.html');
+	d3.select('#bonusamount').select(".bignumber").text('$'+bonus);
+}
+
+checkcodefail = function () {
+	d3.select('.badcode').style("display","inline-block");
+}
+
+checkcode = function() {
+	console.log("checking code");
+
+//	var parts = window.location.search.substr(1).split("&");
+//	var $_GET = {};
+//	for (var i = 0; i < parts.length; i++) {
+//	    var temp = parts[i].split("=");
+//	    $_GET[decodeURIComponent(temp[0])] = decodeURIComponent(temp[1]);
+//	}
+
+
+	$.ajax({
+		dataType: "json",
+		type: "POST",
+		data: {uniqueid: uniqueId, workerid: $_GET['workerId'], code: $("#completecode").val()},
+		url: "/check_flask",
+		success: function(data) {
+			checkcodesuccess(data.bonus);
+		},
+		error: function () {
+			checkcodefail();
+		}
+	});
+}
+
+completehit = function() {
+	psiTurk.completeHIT();
+}
+// Task object to keep track of the current phase
+var currentview;
+
+/*******************
+ * Run Task
+ ******************/
+$(window).load( function(){
+	// Load the stage.html snippet into the body of the page
+	psiTurk.showPage('form.html');
+	$('#myalert').on('click', function () {
+		d3.select('.badcode').style("display","none");
+	});
+
+});
