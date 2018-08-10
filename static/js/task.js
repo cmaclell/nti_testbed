7e7479a07e8910ece97bd962ccdc86ab597fe02e
Chris MacLellan <chris.maclellan@soartech.com>
2018-08-10 17:22:49 -0400

    added questionnaire

--- static/js/task.js
+++ static/js/task.js
@@ -11,6 +11,7 @@ var psiTurk = new PsiTurk(uniqueId, adServerLoc, mode);
 console.log("loading task.js for worker: " + uniqueId);
 
 var pages = [
+    "postquestionnaire.html",
     "stage.html",
     "success.html"
 ];
@@ -62,10 +63,58 @@ do_instructions = function(arg) {
 
 }
 
-var socket = io.connect("ws://localhost:5000");
+var socket = io.connect("ws://" + window.location.host); //'ws://localhost:5000');
 
+socket.on('complete_hit', function(arg) { 
+	var error_message = "<h1>Oops!</h1><p>Something went wrong submitting your HIT. This might happen if you lose your internet connection. Press the button to resubmit.</p><button id='resubmit'>Resubmit</button>";
 
-socket.on('complete_hit', function(arg) { psiTurk.completeHIT() })
+	record_responses = function() {
+
+		psiTurk.recordTrialData({'phase':'postquestionnaire', 'status':'submit'});
+
+		$('textarea').each( function(i, val) {
+			psiTurk.recordUnstructuredData(this.id, this.value);
+		});
+		$('select').each( function(i, val) {
+			psiTurk.recordUnstructuredData(this.id, this.value);		
+		});
+
+	};
+
+	prompt_resubmit = function() {
+		document.body.innerHTML = error_message;
+		$("#resubmit").click(resubmit);
+	};
+
+	resubmit = function() {
+		document.body.innerHTML = "<h1>Trying to resubmit...</h1>";
+		reprompt = setTimeout(prompt_resubmit, 10000);
+		
+		psiTurk.saveData({
+			success: function() {
+			    clearInterval(reprompt); 
+                psiTurk.computeBonus('compute_bonus', function(){
+                	psiTurk.completeHIT(); // when finished saving compute bonus, the quit
+                }); 
+			}, 
+			error: prompt_resubmit
+		});
+	};
+
+    $('body').html('');
+    psiTurk.showPage('postquestionnaire.html');
+    // psiTurk.completeHIT()
+    $("#next").click(function () {
+	    record_responses();
+	    psiTurk.saveData({
+            success: function(){
+                psiTurk.computeBonus('compute_bonus', function() { 
+                	psiTurk.completeHIT(); // when finished saving compute bonus, the quit
+                }); 
+            }, 
+            error: prompt_resubmit});
+	});
+})
 
 
 socket.on('connect', function() {
