4f88ba317e839c8f600b60a77f194cbc81220ed2
Chris MacLellan <chris.maclellan@soartech.com>
2018-08-13 17:02:00 -0400

    added some form validation.

--- static/js/task.js
+++ static/js/task.js
@@ -11,16 +11,17 @@ var psiTurk = new PsiTurk(uniqueId, adServerLoc, mode);
 console.log("loading task.js for worker: " + uniqueId);
 
 var pages = [
-    "postquestionnaire.html",
     "stage.html",
-    "success.html"
+    "success.html",
+    "student-postquestionnaire.html",
+    "teacher-postquestionnaire.html"
 ];
 
 var student_instruction_pages = [
-    "instructions/student-process-and-role.html", 
-    "instructions/student-task-interface.html", 
-    "instructions/student-training-interface-apprentice.html", 
-    "instructions/ready.html", 
+    "instructions/student-process-and-role.html",
+    "instructions/student-task-interface.html",
+    "instructions/student-training-interface-apprentice.html",
+    "instructions/ready.html",
 ];
 
 var teacher_instruction_pages = [
@@ -28,13 +29,11 @@ var teacher_instruction_pages = [
     "instructions/teacher-task-interface.html",
     "instructions/teacher-training-interface-apprentice.html",
     "instructions/teacher-task.html",
-    "instructions/ready.html"
+    "instructions/ready.html",
 ];
 
 
 psiTurk.preloadPages(pages);
-// psiTurk.preloadPages(student_instruction_pages);
-// psiTurk.preloadPages(teacher_instruction_pages);
 
 log = function(data) {
     console.log(data);
@@ -48,93 +47,153 @@ completehit = function(arg) {
 }
 
 do_instructions = function(arg) {
-
     console.log("instruction request for role: " + arg);
+    set_complete_hit_listener(arg);
+
     if (arg == "sandbox") {
         psiTurk.preloadPages(teacher_instruction_pages);
-        psiTurk.doInstructions(teacher_instruction_pages, function() {psiTurk.showPage('stage.html');});
+        psiTurk.doInstructions(teacher_instruction_pages, function() {
+            psiTurk.showPage('stage.html');
+        });
     }
 
     if (arg == "student") {
         psiTurk.preloadPages(student_instruction_pages);
-        psiTurk.doInstructions(student_instruction_pages, function() {psiTurk.showPage('stage.html');});
+        psiTurk.doInstructions(student_instruction_pages, function() {
+            psiTurk.showPage('stage.html');
+        });
     }
 
     if (arg == "teacher") {
         psiTurk.preloadPages(teacher_instruction_pages);
-        psiTurk.doInstructions(teacher_instruction_pages, function() {psiTurk.showPage('stage.html');});
+        psiTurk.doInstructions(teacher_instruction_pages, function() {
+            psiTurk.showPage('stage.html');
+        });
     }
 
 }
 
 var socket = io.connect("ws://" + window.location.host); //'ws://localhost:5000');
 
-socket.on('complete_hit', function(arg) { 
-	var error_message = "<h1>Oops!</h1><p>Something went wrong submitting your HIT. This might happen if you lose your internet connection. Press the button to resubmit.</p><button id='resubmit'>Resubmit</button>";
+function validateForm() {
+    var isValid = true;
+    $('input').each(function(){
+        if ($(this).val() === ''){
+            isValid = false;
+            $(this).parents('.question').addClass('alert-warning');
+        }
+        else{
+            $(this).parents('.question').removeClass('alert-warning');
+        }
+    });
+    $('textarea').each(function(){
+        if ($(this).val() === ''){
+            isValid = false;
+            $(this).parents('.question').addClass('alert-warning');
+        }
+        else{
+            $(this).parents('.question').removeClass('alert-warning');
+        }
+    });
+    return isValid;
 
-	record_responses = function() {
+}
 
-		psiTurk.recordTrialData({'phase':'postquestionnaire', 'status':'submit'});
+function set_complete_hit_listener(role) {
+    socket.on('complete_hit', function(arg) {
+        var error_message = "<h1>Oops!</h1><p>Something went wrong submitting your HIT. This might happen if you lose your internet connection. Press the button to resubmit.</p><button id='resubmit'>Resubmit</button>";
 
-		$('input').each( function(i, val) {
-			psiTurk.recordUnstructuredData(this.id, this.value);		
-		});
-		$('textarea').each( function(i, val) {
-			psiTurk.recordUnstructuredData(this.id, this.value);
-		});
-		$('select').each( function(i, val) {
-			psiTurk.recordUnstructuredData(this.id, this.value);		
-		});
+        record_responses = function() {
 
-	};
+            psiTurk.recordTrialData({
+                'phase': 'postquestionnaire',
+                'status': 'submit'
+            });
 
-	prompt_resubmit = function() {
-		document.body.innerHTML = error_message;
-		$("#resubmit").click(resubmit);
-	};
+            $('input').each(function(i, val) {
+                psiTurk.recordUnstructuredData(this.id, this.value);
+            });
+            $('textarea').each(function(i, val) {
+                psiTurk.recordUnstructuredData(this.id, this.value);
+            });
 
-	resubmit = function() {
-		document.body.innerHTML = "<h1>Trying to resubmit...</h1>";
-		reprompt = setTimeout(prompt_resubmit, 10000);
-		
-		psiTurk.saveData({
-			success: function() {
-			    clearInterval(reprompt); 
-                psiTurk.computeBonus('compute_bonus', function(){
-                	psiTurk.completeHIT(); // when finished saving compute bonus, the quit
-                }); 
-			}, 
-			error: prompt_resubmit
-		});
-	};
+        };
 
-    $('#unityFrame').remove();
+        prompt_resubmit = function() {
+            document.body.innerHTML = error_message;
+            $("#resubmit").click(resubmit);
+        };
 
-    psiTurk.showPage('postquestionnaire.html');
-    // psiTurk.completeHIT()
-    $("#next").click(function () {
-	    record_responses();
-	    psiTurk.saveData({
-            success: function(){
-                psiTurk.computeBonus('compute_bonus', function() { 
-                	psiTurk.completeHIT(); // when finished saving compute bonus, the quit
-                }); 
-            }, 
-            error: prompt_resubmit});
-	});
-})
+        resubmit = function() {
+            document.body.innerHTML = "<h1>Trying to resubmit...</h1>";
+            reprompt = setTimeout(prompt_resubmit, 10000);
 
+            psiTurk.saveData({
+                success: function() {
+                    clearInterval(reprompt);
+                    psiTurk.completeHIT();
+                    // psiTurk.computeBonus('compute_bonus', function(){
+                    //     psiTurk.completeHIT(); // when finished saving compute bonus, the quit
+                    // }); 
+                },
+                error: prompt_resubmit
+            });
+        };
+
+        $('#unityFrame').remove();
+
+        console.log("loading postquestionnaire for role: " + role);
+        if (role == "sandbox") {
+            psiTurk.showPage('teacher-postquestionnaire.html');
+        }
+
+        if (role == "student") {
+            psiTurk.showPage('student-postquestionnaire.html');
+        }
+
+        if (role == "teacher") {
+            psiTurk.showPage('teacher-postquestionnaire.html');
+        }
+
+        // psiTurk.completeHIT()
+        $("#next").click(function() {
+            if (validateForm()){
+                record_responses();
+                psiTurk.saveData({
+                    success: function() {
+                        psiTurk.completeHIT();
+                        // psiTurk.computeBonus('compute_bonus', function() { 
+                        //     psiTurk.completeHIT(); // when finished saving compute bonus, the quit
+                        // }); 
+                    },
+                    error: prompt_resubmit
+                });
+            }
+            else {
+                $('.instructions').append('<div id="all_fields_warning" class="alert alert-warning">All fields are required and some of the fields are incomplete. Please complete them before submitting.</div>');
+            }
+        });
+    });
+}
 
 socket.on('connect', function() {
-        socket.emit('join', {'id':  uniqueId});
+    socket.emit('join', {
+        'id': uniqueId
+    });
 });
 
-
-$(window).load( function(){
-    socket.emit('join', {'id':  uniqueId, 'first': true});
+$(window).load(function() {
+    socket.emit('join', {
+        'id': uniqueId,
+        'first': true
+    });
     //psiTurk.showPage('stage.html');
-    socket.on('instructions', function(arg) { do_instructions(arg) })
+    socket.on('instructions', function(arg) {
+        do_instructions(arg)
+    })
     // socket.on('refresh', function(arg) { psiTurk.showPage('stage.html'); })
-    socket.on('refresh', function(arg) { do_instructions(arg); })
-    
+    socket.on('refresh', function(arg) {
+        do_instructions(arg);
+    })
+
 });
